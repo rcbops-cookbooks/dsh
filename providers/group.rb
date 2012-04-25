2eb7e33a8cbaf7113e28dd44b3a33dde6b6545c1
William Kelly <the.william.kelly@gmail.com>
2012-04-25 02:06:10 -0500

    Remove unnecessary ruby_blocks.  Food critic.

--- providers/group.rb
+++ providers/group.rb
@@ -1,36 +1,32 @@
 action :join do
-  ruby_block "set host key attribute" do
-    block {
-      host_key = ::File.read("/etc/ssh/ssh_host_rsa_key.pub").strip
-      node["dsh"]["host_key"] = host_key
-    }
+  host_key = ::File.read("/etc/ssh/ssh_host_rsa_key.pub").strip
+  if host_key != node["dsh"]["host_key"]
+    node["dsh"]["host_key"] = host_key
+    new_resource.updated_by_last_action(true)
   end
-  ruby_block "add group attribute" do
-    only_if { new_resource.user }
-    block { node["dsh"]["groups"][new_resource.name] = new_resource.user }
+  if new_resource.user and current_resource and 
+      new_resource.user != current_resource.user
+    node["dsh"]["groups"][new_resource.name] = current_resource.user
+    new_resource.updated_by_last_action(true)
   end
-  ruby_block "generate ssh key" do
-    only_if { new_resource.admin_user }
-    block {
-      home = ::File.expand_path "~#{new_resource.admin_user}"
-      privkey_path = "#{home}/.ssh/id_rsa"
-      pubkey_path = "#{privkey_path}.pub"
-      if not (::File.exists? privkey_path or ::File.exists? pubkey_path)
-        #generate ssh key
-        system(" ssh-keygen -q -f #{privkey_path} -P \"\"", :in=>"/dev/null")
-      end
-    }
-  end
-  ruby_block "add admin attribute" do
-    only_if { new_resource.admin_user }
-    block {
-      home = ::File.expand_path "~#{new_resource.admin_user}"
-      pubkey = ::File.read("#{home}/.ssh/id_rsa.pub").strip
+  if new_resource.admin_user
+    home = ::File.expand_path "~#{new_resource.admin_user}"
+    privkey_path = "#{home}/.ssh/id_rsa"
+    pubkey_path = "#{privkey_path}.pub"
+    if not (::File.exists? privkey_path or ::File.exists? pubkey_path)
+      #generate ssh key
+      system("ssh-keygen -q -f #{privkey_path} -P \"\"", :in=>"/dev/null")
+      new_resource.updated_by_last_action(true)
+    end
+    home = ::File.expand_path "~#{new_resource.admin_user}"
+    pubkey = ::File.read("#{home}/.ssh/id_rsa.pub").strip
+    if pubkey != node["dsh"]["admin_groups"][new_resource.name]["pubkey"]
       node["dsh"]["admin_groups"][new_resource.name] = {
         "user" => new_resource.admin_user,
         "pubkey" => pubkey
       }
-    }
+      new_resource.updated_by_last_action(true)
+    end
   end
 end
 
