76b80cd6420dbe010f4eb04baf0b64c104245835
William Kelly <the.william.kelly@gmail.com>
2012-05-01 09:59:06 -0500

    Changed user default to nil

--- resources/group.rb
+++ resources/group.rb
@@ -6,7 +6,7 @@ def initialize(*args)
 end
 
 attribute :group, :kind_of => String, :name_attribute => true
-attribute :user, :kind_of => String, :default => true
+attribute :user, :kind_of => String, :default => nil
 attribute :admin_user, :kind_of => String, :default => nil
 attribute :admin_pubkey, :kind_of => String, :default => nil
 attribute :network, :kind_of => String, :default => nil
