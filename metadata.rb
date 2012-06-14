0b604ce0c0419f9c34a254a6ebeb8101081abfa9
rcbjenkins <rcb-deploy@lists.rackspace.com>
2012-06-14 17:46:57 -0500

    End of sprint version bump.

--- metadata.rb
+++ metadata.rb
@@ -3,5 +3,5 @@ maintainer_email "william.kelly@rackspace.com"
 license          "All rights reserved"
 description      "Installs/Configures dsh"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.2"
+version          "0.0.3"
 depends          "osops-utils"
