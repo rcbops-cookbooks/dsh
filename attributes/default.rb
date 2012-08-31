b9e445642a46e6c39ddd46a5c92d6fdb56b450b8
Joseph W. Breu <breu@breu.org>
2012-08-31 10:54:41 -0500

    attribute classification

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
-default["dsh"]["groups"] = {}
-default["dsh"]["admin_groups"] = {}
-default["dsh"]["host_key"] = ""
-default["dsh"]["hosts"] = []
+default["dsh"]["groups"] = {}               # node_attribute
+default["dsh"]["admin_groups"] = {}         # node_attribute
+default["dsh"]["host_key"] = ""             # node_attribute
+default["dsh"]["hosts"] = []                # node_attribute
