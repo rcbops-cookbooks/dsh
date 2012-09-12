e19da39c9051b2ca46e8699cc4cc42f6a7de1807
Joseph W. Breu <breu@breu.org>
2012-09-12 18:59:29 +0000

    Template pssh package name

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,3 +2,14 @@ default["dsh"]["groups"] = {}               # node_attribute
 default["dsh"]["admin_groups"] = {}         # node_attribute
 default["dsh"]["host_key"] = ""             # node_attribute
 default["dsh"]["hosts"] = []                # node_attribute
+
+case platform
+when "fedora", "redhat", "centos"
+  default["pssh"]["platform"] = {
+    "pssh_packages" => ["pdsh-mod-dshgroup"]
+  }
+when "ubuntu"
+  default["pssh"]["platform"] = {
+    "pssh_packages" => ["pssh"]
+  }
+end
