e90b4e20181efe3d2789d0c591f02d6cc986e70f
rcbjenkins <rcb-deploy@lists.rackspace.com>
2012-10-03 10:56:36 -0500

    End of sprint version bump.

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ maintainer       "Rackspace Us, Inc."
 license          "Apache 2.0"
 description      "Installs and Configures dsh"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.7"
+version          "0.0.8"
 
 %w{ ubuntu fedora }.each do |os|
   supports os
