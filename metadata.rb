0e110c14e147035d4a0f68d6e0bb0e4f0b4b18a7
Joe Breu <breu@breu.org>
2013-10-08 17:56:08 +0000

    version bump

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "rcb-deploy@lists.rackspace.com"
 license          "Apache 2.0"
 description      "Installs and Configures dsh"
 long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
-version          "0.0.17"
+version           "4.2.0"
 
 %w{ amazon centos debian fedora oracle redhat scientific ubuntu }.each do |os|
   supports os
